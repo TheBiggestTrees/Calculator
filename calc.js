afc6ee5bb7ecf84c2343246fdaf6e809d1048fd3
Tiirnako <sithlord229@gmail.com>
2023-09-15 20:29:34 -0500

    Keypress functions added
    Added functionality to press the numpad keys for inputs.

--- calc.js
+++ calc.js
@@ -3,6 +3,7 @@ let answer = document.getElementById('previousAnswer')
 let result;
 let a;
 let b;
+let previousEquation;
 let xFill = false;
 let op = '';
 let x = [];
@@ -21,37 +22,36 @@ function clearScreen() {
 }
 
 function operateReset() {
-    op = '';
-    a = '';
+    a = result;
     b = '';
-    x = [];
+    x = Array.from(String(result));
     y = [];
-    result = '';
+    result = 0;
     xFill = false;
 }
 
 function addBtn() {
     xFill = true;
     op = '+';
-    display.innerHTML = `${a} ${op}`;
+    display.innerHTML = `${a} +`;
 }
 
 function subtractBtn() {
     xFill = true;
-    op = '-';
-    display.innerHTML = `${a} ${op}`;
+    op = '–';
+    display.innerHTML = `${a} –`;
 }
 
 function divideBtn() {
     xFill = true;
-    op = '/';
-    display.innerHTML = `${a} ${op}`;
+    op = '÷';
+    display.innerHTML = `${a} ÷`;
 }
 
 function multiplyBtn() {
     xFill = true;
-    op = '*';
-    display.innerHTML = `${a} ${op}`;
+    op = '×';
+    display.innerHTML = `${a} ×`;
 }
 
 function insertNum(n) {
@@ -68,25 +68,31 @@ function insertNum(n) {
 }
 
 function operate() {
+    previousEquation = `${a} ${op} ${b}`;
+
     switch(op) {
         case '+': 
             result = a + b;
-            answer.innerHTML = result;
+            answer.innerHTML = previousEquation;
+            display.innerHTML = result;
             operateReset();
             break
-        case '-': 
+        case '–': 
             result = a - b;
-            answer.innerHTML = result;
+            answer.innerHTML = previousEquation;
+            display.innerHTML = result;
             operateReset();
             break
-        case '*': 
+        case '×': 
             result = a * b;
-            answer.innerHTML = result;
+            answer.innerHTML = previousEquation;
+            display.innerHTML = result;
             operateReset();
             break
-        case '/': 
+        case '÷': 
             result = a / b;
-            answer.innerHTML = result;
+            answer.innerHTML = previousEquation;
+            display.innerHTML = result;
             operateReset();
             break
         default: 
@@ -97,14 +103,70 @@ function operate() {
 
 function backspace() {
     
-    if (xFill === false) {
+    if(y.length > 0) {
+        y.splice(-1, 1);
+        b = +y.join('')
+        display.innerHTML =  `${a} ${op} ${b}`;     
+    }  else {
         x.splice(-1, 1);
         a = +x.join('');
         display.innerHTML = a;
-    }  else {
-        y.splice(-1, 1);
-        b = +y.join('')
-        display.innerHTML =  `${a} ${op} ${b}`;
     } 
     
+}
+
+window.onkeydown = (e) => {
+    switch(e.keyCode) {
+        case 8:
+            backspace();
+            break
+        case 13:
+            operate();
+            break
+        case 96: 
+            insertNum(0);
+            break
+        case 97: 
+            insertNum(1);
+            break
+        case 98: 
+            insertNum(2);
+            break
+        case 99: 
+            insertNum(3);
+            break
+        case 100: 
+            insertNum(4);
+            break
+        case 101: 
+            insertNum(5);
+            break
+        case 102: 
+            insertNum(6);
+            break
+        case 103: 
+            insertNum(7);
+            break
+        case 104: 
+            insertNum(8);
+            break
+        case 105: 
+            insertNum(9);
+            break
+        case 106:
+            multiplyBtn();
+            break
+        case 107:
+            addBtn();
+            break
+        case 109:
+            subtractBtn();
+            break
+        case 110:
+            insertNum('.');
+            break
+        case 111:
+            divideBtn();
+            break
+    }
 }
